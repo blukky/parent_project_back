f1950b0a65a409256d9ec62fe26736232ed98864
blukky <d.shalkin@bag24.ru>
2024-02-19 21:19:53 +0300

    feat add cors

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,11 +27,11 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(120)->by($request->user()?->id ?: $request->ip());
         });
 
         $this->routes(function () {
-            Route::middleware('api')
+            Route::middleware(['api', 'cors'])
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
